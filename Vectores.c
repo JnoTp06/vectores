309df3ad03dbe89d7b79044622be2336f3a54119
Juliano Toapata <juliano.toapanta@udla.edu.ec>
2025-10-24 13:59:43 -0500

    cambio de valores a valores aleatorios

--- Vectores.c
+++ Vectores.c
@@ -1,36 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define MAX_SIZE 100
 
 int main() {
     int toapanta[MAX_SIZE];
     int n, i;
-    int num;
 
-    
     printf("Ingrese la cantidad de numeros enteros a almacenar (maximo %d): ", MAX_SIZE);
     scanf("%d", &n);
 
-   
     if (n > MAX_SIZE || n <= 0) {
         printf("Cantidad invalida. Debe ser entre 1 y %d.\n", MAX_SIZE);
         return 1;
     }
 
+   
+    srand(time(NULL));
+
+    printf("\nLlenando el arreglo automaticamente con numeros aleatorios entre 0 y 100...\n");
+
     
-    printf("Ingrese %d numeros enteros entre 0 y 100:\n", n);
     for (i = 0; i < n; i++) {
-        do {
-            printf("Numero %d: ", i + 1);
-            scanf("%d", &num);
-
-            if (num < 0 || num > 100) {
-                printf("Error: el numero debe estar entre 0 y 100. Intente de nuevo.\n");
-            }
-
-        } while (num < 0 || num > 100);  
-
-        toapanta[i] = num; 
+        toapanta[i] = rand() % 101; 
     }
 
     
@@ -41,4 +34,5 @@ int main() {
     printf("\n");
 
     return 0;
+
 }
